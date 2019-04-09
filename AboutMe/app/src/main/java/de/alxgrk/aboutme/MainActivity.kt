20655ab2eb4f05fa8169199d7bf4a5146aaead30
Alexander Girke <alexander.girke@mercateo.com>
2019-04-09 22:56:46 +0200

    finishing 2.15

---
+++ AboutMe/app/src/main/java/de/alxgrk/aboutme/MainActivity.kt
@@ -0,0 +1,35 @@
+package de.alxgrk.aboutme
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        val edit = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTV = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTV.text = edit.text
+        edit.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTV.visibility = View.VISIBLE
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
